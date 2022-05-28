23b5e5b337ac20e339ad77cdac8bbc3066f62cab
Prokhor Minin <prokhor@len0xx.space>
2022-05-28 23:46:26 +0500

    Zoom the cover in on mouse over

--- src/lib/components/NewsCard.svelte
+++ src/lib/components/NewsCard.svelte
@@ -5,7 +5,9 @@
 </script>
 
 <div class="news-card">
-    <div class="cover" style="background-image: url({img})"><div class="rainbow"></div></div>
+    <div class="cover-wrapper">
+        <div class="cover" style="background-image: url({img})"><div class="rainbow"></div></div>
+    </div>
     <div class="news-content">
         <Heading size={3} className="blue-text" marginTop={0}>
             <slot name="title" />
@@ -16,18 +18,37 @@
 
 <style>
     .news-card {
-        border: 1px solid rgba(0, 0, 0, 0.1);
-        cursor: pointer;
-    }
-
-    .cover {
         display: block;
         position: relative;
-        max-width: 100%;
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        cursor: pointer;
+        overflow: hidden;
+    }
+
+    .cover-wrapper {
+        display: block;
+        position: relative;
+        width: 100%;
         height: 260px;
+    }
+    
+    .cover-wrapper .cover {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 260px;
+        top: 0;
+        left: 0;
         background-position: center;
-        background-size: cover;
+        background-size: 100%;
         background-repeat: no-repeat;
+        transition: 0.1s ease-in-out;
+        transform: scale(1);
+        z-index: 0;
+    }
+
+    .news-card:hover .cover {
+        transform: scale(1.05);
     }
 
     .news-card .cover .rainbow {
@@ -47,6 +68,8 @@
     } 
 
     .news-content {
+        background-color: white;
+        z-index: 1;
         padding: 1em;
         padding-bottom: 1.5em;
     }

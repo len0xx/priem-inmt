dc49116f57cafd74950a996bff94e36b5061bd4c
Prokhor Minin <prokhor@len0xx.space>
2022-05-28 23:49:51 +0500

    Support colourful background option

--- src/lib/components/Slide.svelte
+++ src/lib/components/Slide.svelte
@@ -4,7 +4,7 @@
     export let img = ''
 </script>
 
-<Grid s={1} m={2} className="kit-slide" justifyItems="start">
+<Grid s={1} m={2} className="kit-slide color-bg" justifyItems="start">
     <Grid m={1}>
         <div class="kit-slide-content">
             <slot />
@@ -17,6 +17,23 @@
 </Grid>
 
 <style>
+    :global(.kit-slide.color-bg) {
+        background-image: url('/img/slider-gradient.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+    }
+
+    @media screen and (max-width: 768px) {
+        :global(.kit-slide.color-bg) {
+            background-image: url('/img/slider-mobile-gradient.jpg');
+        }
+    }
+
+    :global(.kit-slide.color-bg > *) {
+        color: white;
+    }
+
     :global(.kit-slide > *:first-child) {
         padding-left: var(--content-padding-x);
     }
@@ -41,17 +58,23 @@
         position: relative;
         width: 100%;
         height: 100%;
+        min-height: 550px;
         background-position: center;
         background-size: cover;
         background-repeat: no-repeat;
     }
 
     @media screen and (max-width: 768px) {
+        :global(.kit-slide .kit-slide-content) {
+            max-width: 95%;
+        }
+
         :global(.kit-slide > *:first-child) {
             order: 2;
         }
 
         :global(.kit-slide > .kit-slide-img) {
+            min-height: 250px;
             height: 250px;
             width: 100%;
             order: 1;

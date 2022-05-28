abbd95b8c767f7cd2790fa862e5c028c45fc1bc6
Prokhor Minin <prokhor@len0xx.space>
2022-05-28 23:48:26 +0500

    Minor fix

--- src/lib/components/Profession.svelte
+++ src/lib/components/Profession.svelte
@@ -15,7 +15,6 @@
     <h2 class="red-text no-top-margin">Кто такой и чем занимается { title.toLocaleLowerCase() }?</h2>
     <div>
         <h3 class="no-top-margin">{ @html description }</h3>
-        <br />
         <Text opacity={0.6}>по данным портала hh.ru</Text>
         <p><a href="#reg" class="link blue-color" on:click|preventDefault={ linkClicked }>Подать заявку</a></p>
     </div>

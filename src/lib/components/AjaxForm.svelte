28f25da8f5b5f9924268143ef352771a9f019e28
Prokhor Minin <prokhor@len0xx.space>
2022-05-28 23:44:15 +0500

    Bitrix forms

--- src/lib/components/AjaxForm.svelte
+++ src/lib/components/AjaxForm.svelte
@@ -3,17 +3,46 @@
     import { sendWindowAJAX } from '../utilities'
     import type { RESTMethod } from '../types'
 
+    export let id: string = null
     export let action = ''
     export let className = ''
     export let csrfToken = ''
     export let noReset = false
     export let checkOk = true
+    export let bitrix = false
     export let method: RESTMethod = 'POST'
 
     const dispatch = createEventDispatcher()
 
     const handleSubmit = (e) => {
-        const formData = new FormData(e.target)
+        let formData = new FormData(e.target)
+        if (bitrix) {
+            const formOrigin = formData
+            formData = new FormData()
+            const fio = formOrigin.get('fio')
+            const words = fio.toString().split(' ')
+            let [firstname, lastname] = ['', '-']
+            if (words.length) lastname = words[0]
+            if (words.length > 1) firstname = words[1]
+            const vals = {
+                CONTACT_NAME: [firstname],
+                CONTACT_EMAIL: [formOrigin.get('email')],
+                CONTACT_PHONE: [formOrigin.get('phone')],
+                CONTACT_LAST_NAME: [lastname],
+                DEAL_COMMENTS: [formOrigin.get('message')]
+            }
+            formData.append('values', JSON.stringify(vals))
+            formData.append('properties', "{}")
+            formData.append('consents', '{"AGREEMENT_8":"Y"}')
+            formData.append('recaptcha', "undefined")
+            formData.append('timeZoneOffset', "-300")
+            formData.append('id', "28")
+            formData.append('sec', "wbu85c")
+            formData.append('lang', "ru")
+            formData.append('trace', '{"url":"https://inmt-priem.urfu.ru/","device":{"isMobile":false},"tags":{"ts":1649833527,"list":{},"gclid":null},"client":{},"pages":{"list":[["https://inmt-priem.urfu.ru/",1650616161,"Институт новых материалов и технологий"]]},"gid":null,"previous":{"list":[]},"channels":[{"code":"button","value":10}]}')
+            formData.append('entities', "[]")
+            formData.append('security_sign', "undefined")
+        }
         const requestOptions = {
             method,
             data: formData,
@@ -42,6 +71,6 @@
     }
 </script>
 
-<form class={className} {action} {method} on:submit|preventDefault={handleSubmit}>
+<form {id} class={className} {action} {method} on:submit|preventDefault={handleSubmit}>
     <slot></slot>
 </form>

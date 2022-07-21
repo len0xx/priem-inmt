4e6cb5d737e87916b9214587cbdbbbf0a35a32b7
Prokhor Minin <prokhor@len0xx.space>
2022-07-21 21:14:14 +0500

    Fix app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,19 +4,21 @@ import { User } from '../server/src/models/user'
 
 // See https://kit.svelte.dev/docs/types#the-app-namespace
 // for information about these interfaces
-declare namespace App {
-	interface Locals {
-		userid: string;
+declare global {
+	namespace App {
+		interface Locals {
+			userid: string;
+		}
+
+		// interface Platform {}
+
+		interface Session {
+			loggedIn: boolean,
+			token: string,
+			csrf: string,
+			user: User
+		}
+
+		// interface Stuff {}
 	}
-
-	// interface Platform {}
-
-	interface Session {
-		loggedIn: boolean,
-		token: string,
-		csrf: string,
-		user: User
-	}
-
-	// interface Stuff {}
 }

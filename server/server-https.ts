074e77574677fc1704a8f684521626d4e22cf3b6
Prokhor Minin <prokhor@len0xx.space>
2022-08-05 12:58:48 +0500

    Update https server

--- server/server-https.ts
+++ server/server-https.ts
@@ -3,24 +3,20 @@ import https from 'https'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-import mongoose from 'mongoose'
 import express from 'express'
-import testRouter from './src/routes/test.js'
-import authRouter from './src/routes/api/auth.js'
-import signupRouter from './src/routes/api/signup.js'
-import userInfoRouter from './src/routes/api/user-info.js'
+// import testRouter from './src/routes/test.js'
 
 import { handler } from '../build/handler.js'
 
 dotenv.config()
 
-const { APP_PORT, APP_IP, DB_CONNECTION_STRING } = process.env
+const { APP_PORT, APP_IP } = process.env
 // const dev = NODE_ENV === 'development'
 
-mongoose.connect(DB_CONNECTION_STRING)
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('DB connected'))
+// mongoose.connect(DB_CONNECTION_STRING)
+// const db = mongoose.connection
+// db.on('error', error => console.error(error))
+// db.once('open', () => console.log('DB connected'))
 
 const app = express()
 
@@ -29,10 +25,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // Express routes
-app.use('/test', testRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/signup', signupRouter)
-app.use('/api/user-info', userInfoRouter)
+// app.use('/test', testRouter)
 
 // SvelteKit handler
 app.use(handler)
@@ -44,5 +37,3 @@ const options = {
 
 // Use the SSL certificate
 https.createServer(options, app).listen(+APP_PORT, APP_IP, () => console.log('Server runs on ' + APP_IP + ':' + APP_PORT))
-
-// app.listen(+APP_PORT, APP_IP, () => console.log('Server runs on ' + APP_IP + ':' + APP_PORT))

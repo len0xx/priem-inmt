a12bfac36f107505f8228087d41413e01e293aa7
shamsipav <aseevpav@gmail.com>
2022-08-08 20:00:09 +0500

    Declaring types for Feature model

--- server/src/models/feature.ts
+++ server/src/models/feature.ts
@@ -1,7 +1,10 @@
-import { DataTypes, Model } from 'sequelize'
+import { DataTypes, Model, InferAttributes, InferCreationAttributes } from "sequelize";
 import sequelize from '../../db.js'
 
-class Feature extends Model {}
+class Feature extends Model<InferAttributes<Feature>, InferCreationAttributes<Feature>> {
+    declare title: string;
+    declare description: string;
+}
 
 Feature.init({
     title: {

ad38d1ec2ffdca89adc0838735523a351e92bc6b
artkalugin <artkalugin27@gmail.com>
2022-08-08 22:22:53 +0500

    Add video model

---
+++ server/src/services/video.ts
@@ -0,0 +1,27 @@
+import Video from '../models/Video.js'
+import sequelize from '../../db.js'
+import type { Sequelize } from 'sequelize/types'
+
+class VideoService {
+    client: Sequelize
+    model: typeof Video
+
+    constructor(sequelize: Sequelize) {
+        this.client = sequelize
+        this.model = Video
+    }
+
+    async get(id: number) {
+        return await this.model.findOne({ where: { id } })
+    }
+
+    async getAll() {
+        return await this.model.findAll()
+    }
+
+    async create(video: { firstName: string, lastName: string }) {
+        return await this.model.create(video)
+    }
+}
+
+export default new VideoService(sequelize)

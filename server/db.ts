2f5505c57378f578fce8a48db7d0da51be029e74
Prokhor Minin <prokhor@len0xx.space>
2022-08-08 11:58:00 +0500

    Development configuration for sequelize DB connection

--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,31 @@
-import { Sequelize } from 'sequelize'
+import { Options, Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
 
-export default new Sequelize(
-    'postgres',
-    'postgres',
-    '',
-    {
-        host: 'localhost',
+dotenv.config()
+
+const { NODE_ENV, DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env
+const dev = NODE_ENV == 'development'
+
+interface ConnectionConfig {
+    database: string,
+    user: string,
+    password: string,
+    options: Options
+}
+
+const CONFIG: ConnectionConfig = {
+    database: dev ? 'postgres' : DB_NAME,
+    user: dev ? 'postgres' : DB_USER,
+    password: dev ? '' : DB_PASSWORD,
+    options: {
+        host: dev ? 'localhost' : DB_HOST,
         dialect: 'postgres'
     }
+}
+
+export default new Sequelize(
+    CONFIG.database,
+    CONFIG.user,
+    CONFIG.password,
+    CONFIG.options
 )

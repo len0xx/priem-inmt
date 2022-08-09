12e8ba7a9e0d146634639a9cec9cde5d7bed0bf2
Prokhor Minin <prokhor@len0xx.space>
2022-08-09 17:48:18 +0500

    Password validation

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -48,6 +48,28 @@ User.init(
                 },
                 notNull: {
                     msg: 'Пароль является обязательным для заполнения'
+                },
+                containsLowerAndUpperCase(value: string) {
+                    const letters = 'qwertyuiopasdfghjklzxcvbnm'
+                    const lowerLetters = [...letters]
+                    const upperLetters = [...(letters.toUpperCase())]
+                    
+                    let containsLower = false
+                    let containsUpper = false
+                    lowerLetters.forEach(letter => value.includes(letter) ? containsLower = true : null)
+                    upperLetters.forEach(letter => value.includes(letter) ? containsUpper = true : null)
+                    
+                    if (!(containsLower && containsUpper))
+                        throw new Error('Пароль должен содержать заглавные и строчные буквы')
+                },
+                containsNumbers(value: string) {
+                    const numbers = [...'0123456789']
+                    
+                    let containsNumbers = false
+                    numbers.forEach(letter => value.includes(letter) ? containsNumbers = true : null)
+                    
+                    if (!containsNumbers)
+                        throw new Error('Пароль должен содержать хотя бы одну цифру')
                 }
             }
         },

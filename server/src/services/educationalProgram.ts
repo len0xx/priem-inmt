4dedee6bdc7755391e35c561486d2297f08442e1
shamsipav <aseevpav@gmail.com>
2022-08-09 17:19:48 +0500

    Refactoring educationalProgram service

--- server/src/services/educationalProgram.ts
+++ server/src/services/educationalProgram.ts
@@ -1,13 +1,9 @@
 import EducationalProgram from '../models/educationalProgram.js'
-import sequelize from '../../db.js'
-import type { Sequelize } from 'sequelize/types'
 
 class EducationalProgramService {
-    client: Sequelize
     model: typeof EducationalProgram
 
-    constructor(sequelize: Sequelize) {
-        this.client = sequelize
+    constructor() {
         this.model = EducationalProgram
     }
 
@@ -19,22 +15,9 @@ class EducationalProgramService {
         return await this.model.findAll()
     }
 
-    async create(educationalProgram: {
-        title: string,
-        degree: string,
-        price: string,
-        duration: string,
-        languages: string,
-        directions: string,
-        educationModes: string,
-        teacher: object,
-        vacantSpots: object,
-        feedbacks: object,
-        exams: object,
-        text: string
-     }) {
+    async create(educationalProgram: EducationalProgram) {
         return await this.model.create(educationalProgram)
     }
 }
 
-export default new EducationalProgramService(sequelize)
+export default new EducationalProgramService()

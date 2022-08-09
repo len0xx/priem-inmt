9af10236cc0d7138eae13f93f642437762bdbd72
Prokhor Minin <prokhor@len0xx.space>
2022-08-09 18:29:35 +0500

    Define the routes

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,19 @@
 import { Router } from 'express'
 import { verify } from '../controllers/auth/verify.js'
+import { login, logout } from '../controllers/auth/login.js'
+import { signup } from '../controllers/auth/signup.js'
+import { getInfo } from '../controllers/auth/user.js'
 
 const router = Router() // eslint-disable-line new-cap
 
 router.post('/verify', verify)
+
+router.post('/login', login)
+
+router.post('/logout', logout)
+
+router.post('/signup', signup)
+
+router.post('/user', getInfo)
 
 export default router

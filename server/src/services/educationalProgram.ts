978ce156bc7aba8b745e2e4c5eb880e043a280f6
shamsipav <aseevpav@gmail.com>
2022-08-15 21:11:29 +0500

    Update educationalProgram validation

--- server/src/services/educationalProgram.ts
+++ server/src/services/educationalProgram.ts
@@ -57,6 +57,55 @@ class EducationalProgramService extends BaseService<EducationalProgram, Educatio
 
         return await this.model.create(educationalProgram)
     }
+
+    override async updateById(id: number, educationalProgram: EducationalProgramI) {
+        const validateModes = (mode) => {
+            if (!mode[1].vacantSpots.budget || !mode[1].vacantSpots.contract)
+                throw new HTTPError(HTTPStatus.BAD_REQUEST, 'Поля "Количество мест" являются обязательными')
+
+            if (isNaN(mode[1].vacantSpots.budget) || isNaN(mode[1].vacantSpots.contract))
+                throw new HTTPError(HTTPStatus.BAD_REQUEST, 'Поле "Количество мест" должно являться числовым')
+
+            if (!mode[1].price)
+                throw new HTTPError(HTTPStatus.BAD_REQUEST, 'Поле "Стоимость обучения" является обязательным')
+
+            if (!mode[1].duration)
+                throw new HTTPError(HTTPStatus.BAD_REQUEST, 'Поле "Срок обучения" является обязательным')
+
+            if (!mode[1].languages)
+                throw new HTTPError(HTTPStatus.BAD_REQUEST, 'Поле "Язык освоения" является обязательным')
+
+        }
+
+        const educationModes = educationalProgram.educationModes
+        Object.entries(educationModes).map(mode => validateModes(mode))
+
+        const directions = educationalProgram.directions
+
+        if (!directions.join(','))
+            throw new HTTPError(HTTPStatus.BAD_REQUEST, 'Поле "Направление подготовки" является обязательным')
+
+        const degree = educationalProgram.degree
+        const exams = educationalProgram.exams
+
+        if ((degree === DegreeLevel.BACHELOR || degree === DegreeLevel.SPECIALIST) && exams.length === 0)
+            throw new HTTPError(HTTPStatus.BAD_REQUEST, 'Необходимо указать хотя бы один экзамен')
+
+        const teacher = educationalProgram.teacher
+
+        if (!teacher.name)
+            throw new HTTPError(HTTPStatus.BAD_REQUEST, 'Поле "ФИО" руководителя программы является обязательным')
+
+        if (!teacher.caption)
+            throw new HTTPError(HTTPStatus.BAD_REQUEST, 'Поле "Учёные звания" руководителя программы является обязательным')
+
+        const description = educationalProgram.text
+
+        if (description === '<p></p>')
+            throw new HTTPError(HTTPStatus.BAD_REQUEST, 'Поле "Описание программы" является обязательным')
+
+        return await this.update({ where: { id } }, educationalProgram)
+    }
 }
 
 export default new EducationalProgramService()

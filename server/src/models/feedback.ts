bc3834fc518883e3fde2b03346c2134e0516242c
artkalugin <artkalugin27@gmail.com>
2022-08-16 23:08:06 +0500

    Сreate feedback model

---
+++ server/src/models/feedback.ts
@@ -0,0 +1,52 @@
+import { DataTypes, Model, InferAttributes, InferCreationAttributes } from 'sequelize'
+import sequelize from '../../db.js'
+
+class Feedback extends Model<InferAttributes<Feedback>, InferCreationAttributes<Feedback>> {
+    declare title: string
+    declare description: string
+    declare img: string
+    declare text: string
+}
+
+export type FeedbackI = InferAttributes<Feedback>
+
+Feedback.init(
+    {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Пожалуйста, введите имя автора отзыва'
+                },
+                notEmpty: {
+                    msg: 'Пожалуйста, введите имя автора отзыва'
+                }
+            }
+        },
+        description: {
+            type: DataTypes.STRING
+        },
+        img: {
+            type: DataTypes.STRING
+        },
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Поле «Текст отзыва» не может быть пустым'
+                },
+                notEmpty: {
+                    msg: 'Поле «Текст отзыва» не может быть пустым'
+                }
+            }
+        }
+    },
+    {
+        sequelize,
+        modelName: 'Feedback'
+    }
+)
+
+export default Feedback

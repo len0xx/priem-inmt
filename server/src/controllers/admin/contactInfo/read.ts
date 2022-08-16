2e26d3a28d873d011b95617c9f295898d90c67eb
artkalugin <artkalugin27@gmail.com>
2022-08-16 22:27:29 +0500

    Refactor controllers using catchHTTPErrors

--- server/src/controllers/admin/contactInfo/read.ts
+++ server/src/controllers/admin/contactInfo/read.ts
@@ -1,17 +1,9 @@
 import contactInfoService from '../../../services/contactInfo.js'
-import { getErrorDetails, HTTPResponse } from '../../../utilities.js'
+import { catchHTTPErrors, HTTPResponse } from '../../../utilities.js'
 import { HTTPStatus } from '../../../types/enums.js'
 import type { Request, Response } from 'express'
 
-export const readAll = async (_: Request, res: Response) => {
-    try {
-        const contactInfo = await contactInfoService.getById()
-
-        return new HTTPResponse(res, HTTPStatus.SUCCESS, { contactInfo })
-    }
-    catch (err) {
-        console.error(err)
-        const { code, message } = getErrorDetails(err)
-        return new HTTPResponse(res, code, message)
-    }
-}
+export const readAll = catchHTTPErrors(async (_: Request, res: Response) => {
+    const contactInfo = await contactInfoService.getById()
+    return new HTTPResponse(res, HTTPStatus.SUCCESS, { contactInfo })
+})
--- server/src/controllers/admin/contactInfo/update.ts
+++ server/src/controllers/admin/contactInfo/update.ts
@@ -1,21 +1,14 @@
 import contactInfoService from '../../../services/contactInfo.js'
-import { getErrorDetails, HTTPResponse } from '../../../utilities.js'
+import { catchHTTPErrors, HTTPResponse } from '../../../utilities.js'
 import { HTTPStatus } from '../../../types/enums.js'
 import type { Request, Response } from 'express'
 
-export const update = async (req: Request, res: Response) => {
-    try {
-        const { tel, email, directorateAddress, admissionsAddress, vkUrl, tgUrl } = req.body
+export const update = catchHTTPErrors(async (req: Request, res: Response) => {
+    const { tel, email, directorateAddress, admissionsAddress, vkUrl, tgUrl } = req.body
+    const newData = { id: 1, tel, email, directorateAddress, admissionsAddress, vkUrl, tgUrl }
 
-        const newData = { id: 1, tel, email, directorateAddress, admissionsAddress, vkUrl, tgUrl }
-        if (await contactInfoService.getById(1)) await contactInfoService.update({ where: { id: 1 } }, newData)
-        else await contactInfoService.create(newData)
+    if (await contactInfoService.getById(1)) await contactInfoService.update({ where: { id: 1 } }, newData)
+    else await contactInfoService.create(newData)
 
-        return new HTTPResponse(res, HTTPStatus.SUCCESS, newData)
-    }
-    catch (err) {
-        console.error(err)
-        const { code, message } = getErrorDetails(err)
-        return new HTTPResponse(res, code, message)
-    }
-}
+    return new HTTPResponse(res, HTTPStatus.SUCCESS, newData)
+})

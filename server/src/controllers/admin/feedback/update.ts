7296f8d4699f389c102cfbb62899dc49382c3327
artkalugin <artkalugin27@gmail.com>
2022-08-17 14:01:03 +0500

    Add feedback update controller

---
+++ server/src/controllers/admin/feedback/update.ts
@@ -0,0 +1,13 @@
+import feedbackService from '../../../services/feedback.js'
+import { catchHTTPErrors, HTTPResponse } from '../../../utilities.js'
+import { HTTPStatus } from '../../../types/enums.js'
+import type { Request, Response } from 'express'
+import type { FeedbackI } from '../../../models/feedback.js'
+
+export const update = catchHTTPErrors(async (req: Request, res: Response) => {
+    const id = +req.params.id
+    const { name, description, img, text } = req.body
+    const newData: FeedbackI = { name, description, img, text }
+    await feedbackService.updateById(id, newData)
+    return new HTTPResponse(res, HTTPStatus.SUCCESS, 'Отзыв успешно обновлен')
+})

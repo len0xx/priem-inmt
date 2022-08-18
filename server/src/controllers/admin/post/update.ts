d1ee6853b5adbebff9912ea1a5457ea3134bcd11
Prokhor Minin <prokhor@len0xx.space>
2022-08-18 21:32:23 +0300

    Use catchHTTPErrors

--- server/src/controllers/admin/post/update.ts
+++ server/src/controllers/admin/post/update.ts
@@ -1,34 +1,23 @@
 import postService from '../../../services/post.js'
-import { getErrorDetails, HTTPResponse } from '../../../utilities.js'
+import { catchHTTPErrors, HTTPResponse } from '../../../utilities.js'
 import { HTTPStatus } from '../../../types/enums.js'
 import type { Request, Response } from 'express'
 import type { PostI } from '../../../models/post.js'
 
-export const update = async (req: Request, res: Response) => {
-    try {
-        const id = +req.params.id
+export const update = catchHTTPErrors(async (req: Request, res: Response) => {
+    const id = +req.params.id
+    const { title, text } = req.body
+    const links: { text: string, url: string }[] = []
 
-        const { title, text } = req.body
-
-        const links: { text: string, url: string }[] = []
-
-        for (let i = 1; i <= 5; i++) {
-            const text = req.body[`link_text${i}`]
-            const url = req.body[`link_url${i}`]
-            if (url !== undefined && text !== undefined) {
-                links.push({ url, text })
-            }
+    for (let i = 1; i <= 5; i++) {
+        const text = req.body[`link_text${i}`]
+        const url = req.body[`link_url${i}`]
+        if (url !== undefined && text !== undefined) {
+            links.push({ url, text })
         }
-
-        const newData: PostI = { title, text, links }
-
-        await postService.updateById(id, newData)
-
-        return new HTTPResponse(res, HTTPStatus.SUCCESS, 'Публикация успешно обновлена')
     }
-    catch (e) {
-        console.error(e)
-        const { code, message } = getErrorDetails(e)
-        return new HTTPResponse(res, code, message)
-    }
-}
+
+    const newData: PostI = { title, text, links }
+    await postService.updateById(id, newData)
+    return new HTTPResponse(res, HTTPStatus.SUCCESS, 'Публикация успешно обновлена')
+})

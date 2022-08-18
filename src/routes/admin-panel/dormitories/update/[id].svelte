b1bfb53d188e3037fd2aa2fbeffab79b97f52e21
artkalugin <artkalugin27@gmail.com>
2022-08-18 22:10:43 +0500

    Minor fix

--- src/routes/admin-panel/dormitories/update/[id].svelte
+++ src/routes/admin-panel/dormitories/update/[id].svelte
@@ -76,7 +76,7 @@
                     { errorText }
                 </div>
             { /if }
-            <Grid m={2} s={1} alignItems="stretch">
+            <Grid m={2} s={1}>
                 <div>
                     <label>
                         <span class="caption">Название:</span><br />

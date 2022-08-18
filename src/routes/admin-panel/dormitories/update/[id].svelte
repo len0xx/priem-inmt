4f7175954abe881a90bc062d7285e7815c238980
artkalugin <artkalugin27@gmail.com>
2022-08-18 15:06:51 +0500

    Create dormitory update form

---
+++ src/routes/admin-panel/dormitories/update/[id].svelte
@@ -0,0 +1,82 @@
+<script lang="ts" context="module">
+    import type { Load } from '@sveltejs/kit'
+
+    export const load: Load = async ({ fetch, params }) => {
+        const id = params.id
+        const res = await fetch(`http://localhost:8080/api/admin/dormitory/${id}`)
+        const dormitory = (await res.json()).dormitory
+
+        if (res.ok) {
+            return { props: { dormitory } }
+        }
+    }
+</script>
+
+<script lang="ts">
+    import { AjaxForm, Graduate } from '$components'
+    import { slide } from 'svelte/transition'
+    import { Grid } from '$components'
+    import type { DormitoryI } from '../../../../types'
+
+    export let dormitory: DormitoryI
+
+    let success = false
+    let errorText = ''
+    let successText = ''
+
+    const handleSuccess = (event: CustomEvent<{ message: string }>) => {
+        success = true
+        successText = event.detail.message
+    }
+
+    const handleError = (event: CustomEvent<{ error: string }>) => {
+        success = false
+        errorText = event.detail.error
+    }
+</script>
+
+<svelte:head>
+    <title>ИНМТ – Панель администратора</title>
+</svelte:head>
+
+<section class="main-content">
+    <div class="white-block-wide">
+        <h2 class="no-top-margin">Общежития</h2>
+        <h3>Создать новое общежитие</h3>
+        <AjaxForm action="/api/admin/dormitory/{ dormitory.id }" method="PATCH" on:success={ handleSuccess } on:error={ handleError }>
+            { #if success }
+                <p transition:slide={{ duration: 200 }} class="success">{ successText }</p>
+            { /if }
+            { #if errorText }
+                <p transition:slide={{ duration: 200 }} class="error">{ errorText }</p>
+            { /if }
+            <Grid m={2} s={1} alignItems="stretch">
+                <div>
+                    <label>
+                        <span class="caption">Название:</span><br />
+                        <input class="form-control" type="text" name="title" id="title" value={ dormitory.title } required />
+                    </label>
+                    <br />
+                    <br />
+                    <label>
+                        <span class="caption">Адрес:</span><br />
+                        <input class="form-control" type="text" name="address" id="address" value={ dormitory.address } required />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <span class="caption">Изображение:</span><br />
+                        <input class="form-control" type="text" name="img" id="img" value={ dormitory.img } /> <!-- TODO: file upload -->
+                    </label>
+                </div>
+            </Grid>
+            <br />
+            <button class="btn btn-primary">Сохранить</button>
+        </AjaxForm>
+    </div>
+</section>
+<style>
+    label {
+        width: 100%;
+    }
+</style>

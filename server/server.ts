3ae48444e8a50cc03b63fcae88dccc9e9c79e258
artkalugin <artkalugin27@gmail.com>
2022-08-18 14:23:02 +0500

    Add dormitory router

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import express from 'express'
 import path from 'path'
 import { connectDB } from './db.js'
 import authRouter from './src/routes/auth.js'
+import dormitoryRouter from './src/routes/admin/dormitory.js'
 import feedbackRouter from './src/routes/admin/feedback.js'
 import infoRouter from './src/routes/admin/info.js'
 import postRouter from './src/routes/admin/post.js'
@@ -66,6 +67,7 @@ app.use('/static', express.static(path.join(__dirname, 'static')))
 
 // Express routes
 app.use('/api/auth', authRouter)
+app.use('/api/admin/dormitory', dormitoryRouter)
 app.use('/api/admin/feedback', feedbackRouter)
 app.use('/api/admin/info', infoRouter)
 app.use('/api/admin/post', postRouter)
---
+++ server/src/routes/admin/dormitory.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import { create } from '../../controllers/admin/dormitory/create.js'
+import { readAll, readOne } from '../../controllers/admin/dormitory/read.js'
+import { update } from '../../controllers/admin/dormitory/update.js'
+import { del } from '../../controllers/admin/dormitory/delete.js'
+import { requireAuthorization } from '../../middlewares.js'
+
+const router = Router() /* eslint-disable-line */
+
+router.post('/', requireAuthorization('json'), create)
+
+router.get('/', readAll)
+
+router.get('/:id', readOne)
+
+router.patch('/:id', update)
+
+router.delete('/:id', del)
+
+export default router

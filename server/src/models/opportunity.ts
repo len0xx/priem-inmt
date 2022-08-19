3f2064f581ec8b67132638e1d948b9d0c04e46f8
artkalugin <artkalugin27@gmail.com>
2022-08-19 14:17:50 +0500

    Create opportunity model

---
+++ server/src/models/opportunity.ts
@@ -0,0 +1,45 @@
+import { CreationOptional, DataTypes, Model, InferAttributes, InferCreationAttributes } from 'sequelize'
+import sequelize from '../../db.js'
+
+class Opportunity extends Model<InferAttributes<Opportunity, { omit: 'id' }>, InferCreationAttributes<Opportunity>> {
+    declare id: CreationOptional<number>
+    declare title: string
+    declare description: string
+}
+
+export type OpportunityI = InferAttributes<Opportunity, { omit: 'id' }>
+
+Opportunity.init(
+    {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Пожалуйста, введите название'
+                },
+                notEmpty: {
+                    msg: 'Пожалуйста, введите название'
+                }
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Пожалуйста, введите описание возможности'
+                },
+                notEmpty: {
+                    msg: 'Пожалуйста, введите описание возможности'
+                }
+            }
+        }
+    },
+    {
+        sequelize,
+        modelName: 'Opportunity'
+    }
+)
+
+export default Opportunity

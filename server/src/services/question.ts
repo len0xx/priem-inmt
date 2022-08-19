4591bc567b9d7d1fc8f3d85d0d27209bb52525f3
shamsipav <aseevpav@gmail.com>
2022-08-19 18:51:58 +0500

    Minor fix

--- server/src/services/question.ts
+++ server/src/services/question.ts
@@ -1,5 +1,6 @@
 import BaseService from './base.js'
 import Question, { QuestionI } from '../models/question.js'
+import type { IndexesOptions } from 'sequelize'
 
 class QuestionService extends BaseService<Question, QuestionI> {
     constructor() {
@@ -7,7 +8,7 @@ class QuestionService extends BaseService<Question, QuestionI> {
         this.model = Question
     }
 
-    async findAll(options: any) {
+    async findAll(options: IndexesOptions) {
         return await this.model.findAll(options)
     }
 }

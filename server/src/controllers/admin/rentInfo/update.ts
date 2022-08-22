be75bb5b72dbffd3032a246a8660d296add42ad1
artkalugin <artkalugin27@gmail.com>
2022-08-22 14:18:46 +0500

    Add rentInfo controllers

---
+++ server/src/controllers/admin/rentInfo/update.ts
@@ -0,0 +1,15 @@
+import rentInfoService from '../../../services/rentInfo.js'
+import { catchHTTPErrors, HTTPResponse } from '../../../utilities.js'
+import { HTTPStatus } from '../../../types/enums.js'
+import type { Request, Response } from 'express'
+import type { RentInfoI } from '../../../models/rentInfo.js'
+
+export const update = catchHTTPErrors(async (req: Request, res: Response) => {
+    const { heading, subheading, text, tel1, tel2, linkText, linkURL } = req.body
+    const newData: RentInfoI = { id: 1, heading, subheading, text, tel1, tel2, linkText, linkURL }
+
+    if (await rentInfoService.getById(1)) await rentInfoService.update({ where: { id: 1 } }, newData)
+    else await rentInfoService.create(newData)
+
+    return new HTTPResponse(res, HTTPStatus.SUCCESS, 'Информация об&nbsp;альтернативном поселении успешно обновлена')
+})

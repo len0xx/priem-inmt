8526e7ef8b2fb2843cc4af74b1a86d11f993eb4f
artkalugin <artkalugin27@gmail.com>
2022-08-22 15:32:36 +0500

    Rename readAll() to read()

--- server/src/routes/admin/info.ts
+++ server/src/routes/admin/info.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import { readAll } from '../../controllers/admin/contactInfo/read.js'
+import { read } from '../../controllers/admin/contactInfo/read.js'
 import { update } from '../../controllers/admin/contactInfo/update.js'
 import { requireAuthorization } from '../../middlewares.js'
 
 const router = Router() /* eslint-disable-line */
 
-router.get('/contacts', readAll)
+router.get('/contacts', read)
 router.post('/contacts', requireAuthorization('json'), update)
 
 export default router

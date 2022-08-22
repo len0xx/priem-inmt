5a1522157ec896c8eded1bb50b89ecb4bc76a9b3
shamsipav <aseevpav@gmail.com>
2022-08-22 09:41:54 +0500

    Minor fix

--- server/src/controllers/admin/programs/get.ts
+++ server/src/controllers/admin/programs/get.ts
@@ -2,6 +2,7 @@ import educationalProgramService from '../../../services/educationalProgram.js'
 import { catchHTTPErrors, HTTPResponse } from '../../../utilities.js'
 import { HTTPStatus } from '../../../types/enums.js'
 import type { Request, Response } from 'express'
+import { Op } from 'sequelize'
 
 export const get = catchHTTPErrors(async (req: Request, res: Response) => {
     const id = +req.params.id
@@ -28,6 +29,15 @@ export const getAll = catchHTTPErrors(async (req: Request, res: Response) => {
         getProgramsWhereDegree('Специалитет')
     } else if (degree == 'master') {
         getProgramsWhereDegree('Магистратура')
+    } else if (degree == 'bachspec') {
+        const programs = await educationalProgramService.findAll({
+            where: {
+                degree: {
+                    [Op.or]: ['Бакалавриат', 'Специалитет']
+                }
+            }
+        })
+        return new HTTPResponse(res, HTTPStatus.SUCCESS, { programs })
     } else {
         const programs = await educationalProgramService.get()
         return new HTTPResponse(res, HTTPStatus.SUCCESS, { programs })

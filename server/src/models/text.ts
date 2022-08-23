c54e1eb717e35f57830f381a3c7ccd09554844d7
artkalugin <artkalugin27@gmail.com>
2022-08-23 14:45:15 +0500

    Create Text model

---
+++ server/src/models/text.ts
@@ -0,0 +1,31 @@
+import { CreationOptional, DataTypes, Model, InferAttributes, InferCreationAttributes } from 'sequelize'
+import sequelize from '../../db.js'
+
+class Text extends Model<InferAttributes<Text, { omit: 'id' }>, InferCreationAttributes<Text>> {
+    declare id: CreationOptional<number>
+    declare name: string
+    declare content: string
+}
+
+export type TextI = InferAttributes<Text, { omit: 'id' }>
+
+Text.init(
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            unique: true
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Text'
+    }
+)
+
+export default Text

be8b0c7dbd5d865e01966591c633ffa37e05f7af
artkalugin <artkalugin27@gmail.com>
2022-08-24 17:57:07 +0500

    Update feedback controllers

--- server/src/controllers/admin/feedback/create.ts
+++ server/src/controllers/admin/feedback/create.ts
@@ -3,9 +3,18 @@ import { catchHTTPErrors, HTTPResponse } from '../../../utilities.js'
 import { HTTPStatus } from '../../../types/enums.js'
 import type { Request, Response } from 'express'
 
-export const create = catchHTTPErrors(async (req: Request, res: Response) => {
-    const { level, name, description, img, text } = req.body
-    await feedbackService.create( { level, name, description, img, text } )
+enum Page {
+    Bachelor = 'bachelor',
+    Master = 'master',
+}
 
-    return new HTTPResponse(res, HTTPStatus.CREATED, 'Отзыв успешно создан')
+export const create = catchHTTPErrors(async (req: Request, res: Response) => {
+    const { name, description, img, text } = req.body
+    const page = req.query.page
+
+    if (page === Page.Bachelor || page === Page.Master) {
+        await feedbackService.create( { page, name, description, img, text } )
+        return new HTTPResponse(res, HTTPStatus.CREATED, 'Отзыв успешно создан')
+    }
+    return new HTTPResponse(res, HTTPStatus.BAD_REQUEST, 'Неправильный параметр запроса')
 })
--- server/src/controllers/admin/feedback/update.ts
+++ server/src/controllers/admin/feedback/update.ts
@@ -2,12 +2,11 @@ import feedbackService from '../../../services/feedback.js'
 import { catchHTTPErrors, HTTPResponse } from '../../../utilities.js'
 import { HTTPStatus } from '../../../types/enums.js'
 import type { Request, Response } from 'express'
-import type { FeedbackI } from '../../../models/feedback.js'
 
 export const update = catchHTTPErrors(async (req: Request, res: Response) => {
     const id = +req.params.id
-    const { level, name, description, img, text } = req.body
-    const newData: FeedbackI = { level, name, description, img, text }
+    const { name, description, img, text } = req.body
+    const newData = { name, description, img, text }
     await feedbackService.updateById(id, newData)
     return new HTTPResponse(res, HTTPStatus.SUCCESS, 'Отзыв успешно обновлен')
 })

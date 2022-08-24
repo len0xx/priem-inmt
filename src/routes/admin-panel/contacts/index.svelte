c2eb3055c0f54c2b27503c69607904b4406f73bc
artkalugin <artkalugin27@gmail.com>
2022-08-24 19:21:35 +0500

    Add responsible creation form

--- src/routes/admin-panel/contacts/index.svelte
+++ src/routes/admin-panel/contacts/index.svelte
@@ -75,11 +75,40 @@
             <br />
             <button class="btn btn-primary">Сохранить</button>
         </Form>
+        
 
-        {#if responsibles.length < 10}
-            <a href="/admin-panel/responsibles/new"><button type="button" class="btn btn-outline-primary">Создать новое ответственное лицо</button></a>
-        {/if}
-        <h3>Ответственные лица института { responsibles.length ? `(${responsibles.length})` : '' }</h3>
+        <h3>Ответственные лица института{ responsibles.length ? ` (${responsibles.length})` : '' }</h3>
+        { #if responsibles.length < 10 }
+        <Form method="POST" action="/api/admin/responsible" reset={ true } redirect="/admin-panel/contacts">
+            <h3>Создать ответственное лицо института</h3>
+            <Grid m={2}>
+                <div>
+                    <label for="name">ФИО ответственного лица</label><br />
+                    <input class="form-control wide" type="text" name="name" required />
+                </div>
+                <div>
+                    <label for="label">Подпись</label><br />
+                    <input class="form-control wide" type="text" name="label" required />
+                </div>
+                <div>
+                    <label for="phone">Номер телефона</label><br />
+                    <input
+                        class="form-control wide"
+                        type="text" 
+                        use:imask={ phoneMask }
+                        name="phone" required 
+                    />
+                </div>
+                <div>
+                    <label for="email">Адрес электронной почты</label><br />
+                    <input class="form-control wide" type="email" name="email" required />
+                </div>
+            </Grid>
+            <div class="buttons-row">
+                <button class="btn btn-primary">Создать</button>
+            </div>
+        </Form>
+        { /if }
         { #if responsibles.length }
             <Grid s={1} m={2} l={3}>
                 { #each responsibles as responsible }

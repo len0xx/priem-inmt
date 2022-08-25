4590468b3ad27b6cdf442d9674c0e368fc0b1f72
SHAMSI <aseevpav@gmail.com>
2022-08-25 14:08:31 +0500

    Adding a selection option to the carousel form

--- server/src/services/carousel.ts
+++ server/src/services/carousel.ts
@@ -1,10 +1,15 @@
 import BaseService from './base.js'
 import Carousel, { CarouselI } from '../models/carousel.js'
+import type { IndexesOptions } from 'sequelize'
 
 class CarouselService extends BaseService<Carousel, CarouselI> {
     constructor() {
         super()
         this.model = Carousel
+    }
+
+    async findAll(options: IndexesOptions) {
+        return await this.model.findAll(options)
     }
 }
 

74e74d34d574d18c0c4bf9af2c103c42d644cc25
Prokhor Minin <prokhor@len0xx.space>
2022-09-04 14:39:22 +0500

    Fix the error

--- server/src/controllers/admin/responsible/create.ts
+++ server/src/controllers/admin/responsible/create.ts
@@ -4,8 +4,15 @@ import { catchHTTPErrors, HTTPResponse } from '../../../utilities.js'
 import { HTTPStatus } from '../../../types/enums.js'
 import type { Request, Response } from 'express'
 
+const MAX_CONTACTS_AMOUNT = 4
+
 export const create = catchHTTPErrors(async (req: Request, res: Response) => {
     const { name, label, phone, email, img } = req.body
+    const amount = await responsibleService.count()
+
+    if (amount >= MAX_CONTACTS_AMOUNT) {
+        return new HTTPResponse(res, HTTPStatus.BAD_REQUEST, `Превышено максимальное количество контактов (${ MAX_CONTACTS_AMOUNT }). Чтобы создать новый, необходимо удалить один из существующих`)
+    }
 
     let imgURL: string
     if (+img && !isNaN(+img)) {

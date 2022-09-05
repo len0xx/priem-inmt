6419fcc14b50ad5b137698b1acc995ff6d64a4ea
artkalugin <artkalugin27@gmail.com>
2022-09-05 19:18:21 +0500

    Fix education mode selectors

--- src/lib/components/ProgramCardNew.svelte
+++ src/lib/components/ProgramCardNew.svelte
@@ -7,6 +7,12 @@
 
     let activeMode = 0
 
+    enum EduMode {
+        'fullTime' = 'Очно',
+        'partFullTime' = 'Очно-заочно',
+        'partTime' = 'Заочно'
+    }
+
     beforeUpdate(() => {
         const modesLength = Object.keys(program.educationModes).length
         if (activeMode >= modesLength) activeMode = 0
@@ -16,14 +22,14 @@
 <Card variant="grey" color="custom" on:click className="full-height">
     <svelte:fragment slot="header">
         { #each Object.keys(program.educationModes) as mode, i }
-            <SelectButton size="S" variant={ activeMode == i ? 'active' : 'default' } on:click={ () => activeMode = i } color="blue">{ mode }</SelectButton>
+            <SelectButton size="S" variant={ activeMode == i ? 'active' : 'default' } on:click={ () => activeMode = i } color="blue">{ EduMode[mode] }</SelectButton>
         { /each }
     </svelte:fragment>
     <svelte:fragment slot="title">{ program.title }</svelte:fragment>
     <svelte:fragment slot="text">Направления: { program.directions.join(', ') }</svelte:fragment>
     <span slot="left" class="semi-bold">
-        <span class="red-text">{ program.educationModes[Object.keys(program.educationModes)[activeMode]].vacantSpots.budget }</span> <span class="fourty-text-black">бюджет</span>
-        <span class="blue-text" style:margin-left="1em">{ program.educationModes[Object.keys(program.educationModes)[activeMode]].vacantSpots.contract }</span> <span class="fourty-text-black">контракт</span>
+        <span class="red-text">{ program.educationModes[Object.keys(program.educationModes)[activeMode]].vacantSpots.budget }</span>&nbsp;<span class="fourty-text-black" style:margin-right="1em">бюджет</span>
+        <span class="blue-text">{ program.educationModes[Object.keys(program.educationModes)[activeMode]].vacantSpots.contract }</span>&nbsp;<span class="fourty-text-black">контракт</span>
     </span>
     <svelte:fragment slot="right">от { program.educationModes[Object.keys(program.educationModes)[activeMode]].price }₽</svelte:fragment>
 </Card>
